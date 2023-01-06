39fd1056c83efa203cd32ea155406b23980ad18f
yebinleee <lyb2325@gmail.com>
2023-01-06 20:19:19 +0900

    test: integration test(transactional)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -11,17 +11,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-
     private DataSource dataSource;
 
-    // 데이터 베이스 연결 (dataSource 주입)
 
     @Autowired
     public SpringConfig(DataSource dataSource){
         this.dataSource = dataSource;
     }
-
-    // MemberService, MemberRepository 를 스프링 빈에 등록
 
     @Bean
     public MemberService memberService(){
@@ -30,7 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        System.out.println("datasource에서 데이터를 가져옵니다 : " + dataSource);
         // return new MemoryMemberRepository();
         return new JdbcMemberRepository(dataSource);
     }
