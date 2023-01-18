675b02499c7268409ef1400002e99f4fb2cb56e7
yebinleee <lyb2325@gmail.com>
2023-01-18 10:29:46 +0900

    jpa - entity manager

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 @SpringBootTest
 @Transactional
 class MemberServiceIntegrationTest {
@@ -21,32 +22,31 @@ class MemberServiceIntegrationTest {
      */
     @Test
     void 회원가입() { // 테스트 메서드 이름은 한글로 작성
-        // given
+        // Given
         Member member = new Member();
         member.setName("hello");
 
-        // when
+        // When
         Long saveId = memberService.join(member);
 
-        // then
+        // Then
         Member findMember = memberService.findOne(saveId).get();
         Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
     }
 
     @Test
     void 중복회원_예외(){
-        // given
+        // Given
         Member member1 = new Member();
         member1.setName("hello");
 
         Member member2 = new Member();
         member2.setName("hello");
 
-        // when
+        // When
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
-        // then
         Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 }
