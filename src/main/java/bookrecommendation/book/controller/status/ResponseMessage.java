1841588cb4a57aeafa1270de4fb5e1c6be7b4e39
yebinleee <lyb2325@gmail.com>
2023-02-21 14:48:11 +0900

    add: Response Status Code와 Message 추가 - ResponseEntity에서 body값으로 보낼 status code, status message 추가

---
+++ src/main/java/bookrecommendation/book/controller/status/ResponseMessage.java
@@ -0,0 +1,10 @@
+package bookrecommendation.book.controller.status;
+
+public class ResponseMessage {
+    public static final String LOGIN_SUCCESSFUL = "인증 성공";
+    public static final String LOGIN_FAILED = "인증 실패";
+    public static final String NOT_FOUND_USER = "회원을 찾을 수 없습니다.";
+    public static final String INTERNAL_SERVER_ERROR = "서버 내부 에러";
+    public static final String DB_ERROR = "데이터 베이스 에러";
+    public static final String API_CALL_ERROR = "API 호출 실패";
+}
